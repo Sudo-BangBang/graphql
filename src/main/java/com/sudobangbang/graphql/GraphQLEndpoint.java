8403a4579b53f19a5d22260a1cf2fe8668f4227a
Sudo-BangBang <s.j.tay@outlook.com>
2017-12-26 19:46:37 +0000

    Added endpoint

---
+++ src/main/java/com/sudobangbang/graphql/GraphQLEndpoint.java
@@ -0,0 +1,18 @@
+package com.sudobangbang.graphql;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+import graphql.servlet.SimpleGraphQLServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(SchemaParser.newParser()
+                .file("schema.graphqls") //parse the schema file created earlier
+                .build()
+                .makeExecutableSchema());
+    }
+}
+
